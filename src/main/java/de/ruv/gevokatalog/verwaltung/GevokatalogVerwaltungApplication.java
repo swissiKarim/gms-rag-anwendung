842862137a5b9f26298b22067def1246fc386737
swissi <mohamed.swissi.ext@andsafe.de>
2026-03-11 12:49:28 +0100

    Initial setup for GeVo-Katalog Import application with basic HTML frontend, Spring Boot backend, and H2 configuration

---
+++ src/main/java/de/ruv/gevokatalog/verwaltung/GevokatalogVerwaltungApplication.java
@@ -0,0 +1,11 @@
+package de.ruv.gevokatalog.verwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GevokatalogVerwaltungApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GevokatalogVerwaltungApplication.class, args);
+    }
+}
